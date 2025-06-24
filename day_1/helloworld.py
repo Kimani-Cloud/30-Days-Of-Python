aeccfcf8693a2dc89c9590f8030bbaf905bf2822
Kimani-Cloud <pakymaney85@gmail.com>
2025-06-24 23:56:55 +0300

    Introduction

---
+++ day_1/helloworld.py
@@ -0,0 +1,54 @@
+# Question 1: Print Python version 
+import sys
+print("Python version:", sys.version)
+
+# Question 2: Arithmetic operations
+print(3 + 4) # Addition
+print(3 - 4) # Subtraction
+print(3 * 4) # Multiplication
+print(3 % 4) # Modulus
+print(3 / 4) # Division
+print(3 ** 4) # Exponentiation
+print(3 // 4) # Floor Division
+
+# Question 3: Strings
+print("Paul")
+print("Maina")
+print("Kenya")
+print("I am enjoying 30 days of python")
+
+# Question 4: Data types
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4 - 4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type('Paul'))
+print(type('Maina'))
+print(type('Kenya'))
+
+# Level 3: Data types
+
+integer = 10 # Integer
+
+float = 3.14 # Float
+
+complex = 2 + 3j # Complex number
+
+name = "Paul" # String
+
+PythonisFun = True # Boolean
+
+skills = ['Python', 'Git', 'Streamlit'] # List
+
+coordinates = (10.0, 20.5) # Tuple
+
+numbers = {1, 2, 3, 4, 5} # Set
+
+# Dictionary
+person = {
+    'name': 'Paul',
+    'country': 'Kenya',
+    'skills': ['Python', 'Git']
+}
+
